5025db7f9d3ad3c9e73237c7025cea34893d9b2b
John Martinez <johnmartinezolarte@gmail.com>
2023-02-21 00:12:08 -0500

    completed

--- encapsulation.js
+++ encapsulation.js
@@ -16,3 +16,28 @@ const collegeOne = new College('UIS');
 collegeOne.addTeacher({name: 'Carlos'});
 
 console.log(collegeOne.getTeachers());
+
+
+
+//Another way
+/* class College{
+    constructor(name) {
+        this.name = name;
+    }
+
+    #teachers = [];
+
+    getTeachers() {
+        return this.#teachers;
+    }
+
+    addTeacher(teacher) {
+        this.#teachers.push(teacher);
+    }
+}
+
+const collegeOne = new College('UPTC');
+
+collegeOne.addTeacher({name: 'Freddy'});
+
+console.log(collegeOne.getTeachers()); */
